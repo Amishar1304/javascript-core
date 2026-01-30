c49b35bb25e7955e67156e74aabbeeabde88a998
Amisha <129410909+Amishar1304@users.noreply.github.com>
2026-01-30 16:12:24 +0000

    basic things

---
+++ 01_basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 144523
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+//never use var in code instead of let because of block and functional issues.
+
+// accountId = 536336 // not allowed
+
+
+accountEmail = "mno@hc.com"
+accountPassword = "123414268"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+

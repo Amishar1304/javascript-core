a2cdd969fe588ab634fa34d7e9bff961a6aa866f
Amisha <129410909+Amishar1304@users.noreply.github.com>
2026-01-30 16:40:42 +0000

    Conversion of Datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "35abc"
+console.log(typeof score);
+//can write like this also
+console.log(typeof (score));
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);  //nan[not a number]
+
+let isLoggedIn = "hgdui" //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 = true , 0 = false , " " =  false , "hjdyu" = true 
+*/
+
+let someNum = 33
+let stringNumber = String(someNum)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //string
+
+
+
+
+

f1a4274ae38ee16715fe296c58e8c78c96253048
Amisha <129410909+Amishar1304@users.noreply.github.com>
2026-01-30 17:26:44 +0000

    Conversion of DataTypes and Operations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -21,6 +21,36 @@ let stringNumber = String(someNum)
 console.log(stringNumber); //33
 console.log(typeof stringNumber); //string
 
+//---------------------Operations------------------
+
+let value = 3
+let NegValue =-value
+console.log(NegValue);
+
+console.log(2+2);
+console.log(2**2);
+console.log(2%3);
+
+let str1 ="hello"
+let str2 = "world"
+
+let str = str1 + str2
+console.log(str);
+
+console.log("1" + 2); //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2); //122
+console.log(2 + 3 + "2"); //52
+
+let a = 100
+++a; //101
+//a++;  //101
+console.log(a);
+
+
+
+
+
 
 
 
